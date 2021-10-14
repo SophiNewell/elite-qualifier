8d42a55388c91d468f28a019a1041ed72dfd6a86
SophiNewell <90424761+SophiNewell@users.noreply.github.com>
2021-10-14 22:05:14 +0000

    continued with new code

--- main.py
+++ main.py
@@ -14,13 +14,25 @@ with open("intents.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
   for pattern in intent["patterns"]:
     wrds = nltk.tokenize(pattern)
     words.extend(wrds)
-    docs.append(pattern)
+    docs_x.append(pattern)
+    docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
       labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(classes))]
